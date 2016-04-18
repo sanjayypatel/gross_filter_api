b099ef313d28ef6934dad56f45fa2a5a79c1f5bb
Sanjay Patel <sanjay.y.patel@gmail.com>
2016-04-18 11:05:29 -0700

    Sets the routes contraints for the api

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
 
+  # API definition
+  namespace :api, defaults: { format: :json },
+                  constraints: { subdomain: 'api' },
+                  path: '/'  do
+    # Resources listed here
+  end
+
 end
